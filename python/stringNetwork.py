7f23a129f03c1481b6f4bb70e818bada38306ee6
Sylast <0gageblack0@gmail.com>
2014-09-17 14:52:57 -0400

    added the LevenNumber function

--- python/stringNetwork.py
+++ python/stringNetwork.py
@@ -1,15 +1,42 @@
 #!/usr/bin/python
 import numpy as np
+import sys
 
-def LevenNumber( s1, s2 ):
-	if s1 == s2:
+#stringNetwork [word] [wordlist.txt]
+def LevenNumber( word, target ):
+	if word == target:
 		return 0
-	if len(s1) == 0:
-		return len(s2)
-	if len(s2) == 0:
-		return len(s1)
+	if word == 0:
+		return len(target)
+	if target == 0:
+		return len(word)
 
-	m = np.matrix()
-	print m
+	m = np.array([[0],[0]])
+	m.resize( (len(word), len(target)) )
 
-LevenNumber("word", "words")
+	#Start matrix bounds to 1,2,3...
+	#The word or its target can be reached by 
+	#removing or adding chars
+	for i in range(1, len(word)):
+		m[i,0] = i
+	for j in range(1, len(target)):
+		m[0,j] = j
+	#print m
+
+	for j in range(1,len(target)):
+		for i in range(1,len(word)):
+			if word[i] == target[j]: 		#Letters are the same
+				m[i,j] = m[i-1,j-1]		#No operation needed
+			else:
+				delet 	= m[i-1, j] + 1
+				insert 	= m[i, j-1] + 1
+				sub 	= m[i-1, j-1] + 1
+				#Pick the smallest one
+				m[i,j] = np.min( [delet, insert, sub] )
+
+			#print m
+	return m[len(word)-1, len(target)-1]
+
+#file word.txt = open(argv[1], 'r')
+#wordList = [sys.argv[0]]
+
